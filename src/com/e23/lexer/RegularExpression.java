07500fa807464f3c56259565e852f03338e1d5f7
JianhuanZhuo <keepfight_zjh@163.com>
2015-11-13 18:05:40 +0800

    add Regular Expression

---
+++ src/com/e23/lexer/RegularExpression.java
@@ -0,0 +1,23 @@
+package com.e23.lexer;
+
+import java.util.List;
+import java.util.Queue;
+
+public class RegularExpression {
+	
+	//存储结构
+	
+	Queue<RegularExpression> qu;
+	
+	/**
+	 * 查找
+	 */
+	
+	/**
+	 * 转换
+	 */
+	
+	/**
+	 * 解析字符串为正则式
+	 */
+}
